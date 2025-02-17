4ab735a7c7035cf3d33f7f63d48b71cef415a6df
ORHarrinanan <159671333+ORHarrinanan@users.noreply.github.com>
2025-02-17 19:58:07 -0400

    Add files via upload

--- models.py
+++ models.py
@@ -1,26 +1,92 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.expression import func
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash
-from app import app
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # Replace with your DB URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    return app
+
+app = create_app()
+
+todo_category = db.Table(
+    'todo_category',
+    db.Column('todo_id', db.Integer, db.ForeignKey('todo.id'), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary_key=True),
+    db.Column('last_modified', db.DateTime, default=func.now(), onupdate=func.now())
+)
 
 class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(80), unique=True, nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
-  password = db.Column(db.String(120), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
-  def __init__(self, username, email, password):
-    self.username = username
-    self.email = email
-    self.set_password(password)
+    todos = db.relationship('Todo', backref='user', lazy=True, cascade="all, delete-orphan")
+    categories = db.relationship('Category', backref='user', lazy=True)
 
-  def set_password(self, password):
-    """Create hashed password."""
-    self.password = generate_password_hash(password, method='scrypt')
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.set_password(password)
 
-  def __repr__(self):
-    return f'<User {self.id} {self.username} - {self.email}>'
+    def set_password(self, password):
+        """Create hashed password."""
+        self.password = generate_password_hash(password, method='scrypt')
+
+    def __repr__(self):
+        return f'<User {self.id} {self.username} - {self.email}>'
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    text = db.Column(db.String(255), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+
+    user = db.relationship('User', backref='todo_list')
+    categories = db.relationship('Category', secondary=todo_category, backref='todos')
+
+    def __init__(self, user_id, text):
+        self.user_id = user_id
+        self.text = text
+
+    def toggle(self):
+        self.done = not self.done
+        db.session.commit()
+
+    def __repr__(self):
+        user_name = self.user.username if self.user else "Unknown User"
+        return f'<Todo: {self.id} | User: {user_name} | {self.text} | {"done" if self.done else "not done"}>'
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    text = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, user_id, text):
+        self.user_id = user_id
+        self.text = text
+
+    def __repr__(self):
+        return f'<Category user_id:{self.user_id} - {self.text}>'
+
+                         
+                         
+                         
+
+                     
+                              
+                              
+
+
+                         
+                         
+                           
+                           
--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,100 @@
-import click, sys
-from models import db, User
+import click
+from models import db, User, Todo, Category
 from app import app
 from sqlalchemy.exc import IntegrityError
 
-
 @app.cli.command("init", help="Creates and initializes the database")
 def initialize():
-  db.drop_all()
-  db.init_app(app)
-  db.create_all()
-  bob = User('bob', 'bob@mail.com', 'bobpass')
-  print(bob)
-  print('database intialized')
+    db.drop_all()
+    db.create_all()
+
+    bob = User(username='bob', email='bob@mail.com', password='bobpass')
+    db.session.add(bob)
+    db.session.commit()  # ✅ Make sure Bob is saved before adding Todos
+
+    new_todo = Todo(user_id=bob.id, text='wash car')
+    db.session.add(new_todo)
+    db.session.commit()
+
+    print(bob)
+    print('Database initialized.')
+
+@app.cli.command("get-user", help="Retrieves a User")
+@click.argument('username', default='bob')
+def get_user(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        print(f'{username} not found!')
+        return
+    print(user)
+
+@app.cli.command('get-users')
+def get_users():
+    users = User.query.all()
+    for user in users:
+        print(user)  # ✅ Print users properly
+
+@app.cli.command("change-email")
+@click.argument('username', default='bob')
+@click.argument('email', default='bob@mail.com')
+def change_email(username, email):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        print(f'{username} not found!')
+        return
+    user.email = email
+    db.session.commit()
+    print(user)
+
+@app.cli.command('create-user')
+@click.argument('username', default='rick')
+@click.argument('email', default='rick@mail.com')
+@click.argument('password', default='rickpass')
+def create_user(username, email, password):
+    new_user = User(username=username, email=email, password=password)
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        print("Username or email already taken!")
+    else:
+        print(new_user)
+
+@app.cli.command('delete-user')
+@click.argument('username', default='bob')
+def delete_user(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        print(f'{username} not found!')
+        return
+    db.session.delete(user)
+    db.session.commit()
+    print(f'{username} deleted')
+
+@app.cli.command('get-todos')
+@click.argument('username', default='bob')
+def get_user_todos(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        print(f'{username} not found!')
+        return
+    for todo in user.todos:
+        print(todo)
+
+@app.cli.command('add-todo')
+@click.argument('username', default='bob')
+@click.argument('text', default='wash car')
+def add_task(username, text):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        print(f'{username} not found!')
+        return
+    new_todo = Todo(user_id=user.id, text=text)  # ✅ Fixed user_id
+    db.session.add(new_todo)
+    db.session.commit()
+    print(f'Task "{text}" added to {username}')
+
+@app.cli.command('toggle-todo')
+@click.argument('todo_id', default=1)
+@click
